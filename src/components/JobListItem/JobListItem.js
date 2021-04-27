01e528190af889cf78a039c6b3ae39f065d2df3e
Seongsangcho <liasqui31@gmail.com>
2021-04-27 17:00:30 +0900

    feat: List Item 컴포넌트 작성
    - a tag hover, active는 추후에 결정
    
    todo - 더미데이터 기반 출력

--- src/components/JobListItem/JobListItem.js
+++ src/components/JobListItem/JobListItem.js
@@ -1,32 +1,37 @@
-import React from "react";
-import { JobListItemStyled, DateBox } from "./JobListItem.styles";
+import React, { useState } from "react";
+import Heart from "react-animated-heart";
+import { JobListItemStyled, DateBox, CompanyLogoBox, PostContentSection, PostContent, CompanyInfoBox, HashTagBox } from "./JobListItem.styles";
+import CompanyLogo from "../../images/nong.png";
 
 const JobListItem = props => {
+  const [isClick, setClick] = useState(false);
   return (
     <JobListItemStyled>
       <DateBox>
         <span>D-10</span>
         <span>05.05 (수)</span>
       </DateBox>
-      <div>
-        itemcontentcontainer
-        <div>logo</div>
-        <div>
-          content
-          <span>title</span>
-          <a>compony title</a>
-          <span>location</span>
-          <span>jobtype</span>
-          <span>edu</span>
-          <div>
-            etc info
-            <a>hash1</a>
-            <a>hash2</a>
-            <a>hash3</a>
-          </div>
-        </div>
-      </div>
-      <div>0</div>
+      <PostContentSection name="content-container">
+        <CompanyLogoBox name="logo-container">
+          <img src={CompanyLogo}></img>
+        </CompanyLogoBox>
+        <PostContent name="content">
+          <span>프론트엔드 개발자로 활약하실 분 모집</span>
+          <CompanyInfoBox>
+            <a>농담곰 컴퍼니</a>
+            <span>서울 강남구</span>
+            <span>인턴</span>
+            <span>학력무관</span>
+          </CompanyInfoBox>
+          <HashTagBox>
+            <a>#퍼블리싱</a>
+            <a>#프론트엔드</a>
+            <a>#채용시까지</a>
+            <a>#경력 3년 이하</a>
+          </HashTagBox>
+        </PostContent>
+        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
+      </PostContentSection>
     </JobListItemStyled>
   );
 };
--- src/components/JobListItem/JobListItem.styles.js
+++ src/components/JobListItem/JobListItem.styles.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
-export const JobListItemStyled = styled.div`
+export const JobListItemStyled = styled.li`
   display: flex;
   flex-direction: row;
   background-color: beige;
   width: 100%;
   min-height: 100px;
   font-size: 26px;
+  margin: 10px 0 10px 0;
 `;
 
 export const DateBox = styled.div`
@@ -25,5 +26,55 @@ export const DateBox = styled.div`
   span:nth-child(2) {
     font-size: 0.5em;
     padding-top: 7px;
+  }
+`;
+
+export const PostContentSection = styled.div`
+  display: flex;
+  margin-left: 10px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CompanyLogoBox = styled.div`
+  flex: 1;
+  max-width: 70px;
+  margin: 10px;
+  img {
+    width: 100%;
+  }
+`;
+export const PostContent = styled.div`
+  flex: 1;
+  span:first-child {
+    font-weight: bold;
+    display: block;
+    font-size: 0.8em;
+  }
+`;
+
+export const CompanyInfoBox = styled.div`
+  font-size: 0.5em;
+  margin: 6px 0 6px 0;
+  & * {
+    color: #4a4a4a;
+    padding-right: 5px;
+    margin-right: 5px;
+    border-right: 1px solid black;
+  }
+  & > :first-child {
+    font-weight: bold;
+  }
+  & > :last-child {
+    border: none;
+  }
+`;
+
+export const HashTagBox = styled.div`
+  font-size: 0.5em;
+  & * {
+    color: #27babb;
+    padding-right: 10px;
   }
 `;
