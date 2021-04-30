4608609f91a8e944f24404a1d462366118f0ec05
leejiwonn <bbongwa@naver.com>
2021-05-01 01:27:12 +0900

    fix: Detail 스타일 수정

--- src/containers/DetailContainer.js
+++ src/containers/DetailContainer.js
@@ -1,18 +1,45 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import JobDetailView from "../components/JobDetailView/JobDetailView";
+import CompanyLogo from "../images/nong.png";
 
-const DetailContainer = () => {
+const DetailContainer = props => {
+  const { infoData } = props;
+
   return (
     <>
       <Header />
       <div className="content-section">
         <div className="content-container">
-          <JobDetailView />
+          <JobDetailView infoData={infoData} imgsrc={CompanyLogo} />
         </div>
       </div>
     </>
   );
+};
+
+DetailContainer.defaultProps = {
+  infoData: {
+    id: 1,
+    title: "프론트엔드 개발자로 활약하실 분 모집",
+    company: "농담곰 컴퍼니",
+    dueDate: "05.07 (수)",
+    tags: ["퍼블리싱", "프론트엔드", "채용시까지"],
+    bookmark: true,
+  },
+  dummyData: {
+    id: 1,
+    companyId: 1,
+    title: "프론트엔드 개발자로 활약하실 분 모집",
+    pay: "회사 내규에 따름",
+    tag: ["퍼블리싱", "프론트엔드", "채용시 까지"],
+    type: "인턴",
+    createdDate: "20210420",
+    modifiedDate: "20210427",
+    dueDate: "20210507",
+    content: "대충 이러 저러한 내용들로 구성되어 있다.",
+    isClosed: false,
+  },
 };
 
 export default DetailContainer;
