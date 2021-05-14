82fa8a20e68354404dcc9fd3ecaa727454088fd5
Seongsangcho <liasqui31@gmail.com>
2021-05-14 20:50:44 +0900

    fix: api요청 url변경, withcredential 추가

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://career42gn.tk:8080",
+  // withCredentials: true,
 });
 
 export default api;
--- src/components/LoginView/LoginView.js
+++ src/components/LoginView/LoginView.js
@@ -6,7 +6,7 @@ import { ReactComponent as LogoIcon } from "../../images/logo.svg";
 const LoginView = () => {
   const handleLoginButton = () => {
     window.location.replace(
-      "https://api.intra.42.fr/oauth/authorize?client_id=566e7e201da2842fddb2e21b8dba40467ea7b67d25796020feb9f05c9c7f2581&redirect_uri=http%3A%2F%2F34.64.149.166%3A8080%2F42oauth&response_type=code",
+      "https://api.intra.42.fr/oauth/authorize?client_id=566e7e201da2842fddb2e21b8dba40467ea7b67d25796020feb9f05c9c7f2581&redirect_uri=https%3A%2F%2Fcareer42gn.tk%3A8080%2F42oauth&response_type=code",
     );
   };
 
